da32c6deffa4256682333fbf55dec7783e25977a
Laggerinho <elagger@students.unibe.ch>
2020-05-07 22:28:17 +0200

    path

--- dataloader.py
+++ dataloader.py
@@ -90,11 +90,11 @@ class GenericDataset(data.Dataset):
         self.num_imgs_per_cat = num_imgs_per_cat
 
         if self.dataset_name=='imagenet':
-            assert(self.split=='ILSVRC2012_img_train' or self.split=='ILSVRC2012_img_val')
+            assert(self.split=='ilsvrc2012_img_train' or self.split=='ilsvrc2012_img_val')
             self.mean_pix = [0.485, 0.456, 0.406]
             self.std_pix = [0.229, 0.224, 0.225]
 
-            if self.split!='ILSVRC2012_img_train':
+            if self.split!='ilsvrc2012_img_train':
                 transforms_list = [
                     transforms.Scale(256),
                     transforms.CenterCrop(224),
