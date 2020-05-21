203dfa14b6c9dd3debe4cc5fbc0f01c391209464
Laggerinho <elagger@students.unibe.ch>
2020-05-21 02:54:59 +0200

    changed path to folder for data

--- gpu2/main.py
+++ gpu2/main.py
@@ -1,7 +1,6 @@
+from __future__ import print_function
 import os
 os.environ['CUDA_VISIBLE_DEVICES']='1'
-
-from __future__ import print_function
 import argparse
 import imp
 import algorithms as alg
